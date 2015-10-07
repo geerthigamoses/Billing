282f34b19e4ae6ba6ef3af2823d82afc5f0a4b29
geerthigamoses <geeraueee@gmail.com>
2015-10-07 13:57:54 +0530

    template added first commit

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var cons = require('consolidate');
+//var dust = require('dustjs-helpers');
 var app = express();
-
+app.engine('dust',cons.dust);
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public', {redirect: false}));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +24,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
